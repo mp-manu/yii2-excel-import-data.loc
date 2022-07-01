e68f83e478054343daa9c5fbacf50f0dd14576f6
ManuchehrM <muzafarov9797@gmail.com>
2022-07-01 16:19:08 +0500

    update index.php

--- views/site/index.php
+++ views/site/index.php
@@ -9,6 +9,6 @@ $this->title = 'My Yii Application';
     <div class="jumbotron text-center bg-transparent">
         <h1 class="display-4">Добро пожаловать!</h1>
         <p class="lead">с историей импортов, сделать вывод версий импортов (дата и время когда был импорт), при клике на любой импорт из этого списка открывается таблица, основанная на kartik/GridView (ищется гуглом этот модуль) с выводом всех колонок и возможность опять же выгрузить эту историю в любом формате, что предоставляет kartik/GridView.</p>
-        <p><a class="btn btn-lg btn-success" href="/import-data">Сделать импорт XLS(x) файла</a></p>
+        <p><a class="btn btn-lg btn-success" href="/history">Сделать импорт XLS(x) файла</a></p>
     </div>
 </div>
